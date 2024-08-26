915bbec801db6d31c745364540eaf4b12d47af11
Darron Manning <darronmanning@yahoo.com>
2024-08-26 10:39:42 -0500

    finished methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
+            
+            Console.WriteLine(Add(2,4)); 
+            Console.WriteLine(Sub(13,5));
+            Console.WriteLine(Mult(12,12));
+            Console.WriteLine(Div(144,12));
         }
-    }
+
+        public static void MadLib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello,{userName}! what is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("Favorite band?");
+            string favBand = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine(
+                $"One day, {userName} was walking through the woods while wearing a {favColor} t-shirt.{userName} saw a {favAnimal} listening to {favBand}");
+        }
+
+        public static int Add (int a,int b)
+         {
+             return a + b;
+         }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b; 
+        }
+
+        public static int Mult(int a, int b)
+        {
+            return a * b; 
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a / b; 
+        }
+        
 }
+    
+    
+    
+    
+
+    
+
+
+
+
+}
+
